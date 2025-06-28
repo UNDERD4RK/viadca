c513f55a1c9c992058aa0bb3f7d11d9db3449135
jscamposx <jscamposx@gmail.com>
2025-06-28 15:05:14 -0700

    Empezando a crear el componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-import './App.css'
+import './App.css';
+import NavBar from './componentes/NavBar';
 
 function App() {
-
-
   return (
     <>
-      <h1>Demo Viaje</h1>
-      <p>Esta es una demostración de un viaje.</p>
-      <p>¡Disfruta de la experiencia!</p>
-      <p>Visita nuestro sitio web para más información.</p>
-      <a href="https://campos.alwaysdata.net" target="_blank" rel="noopener noreferrer">Visitar sitio web</a>
-       <Button variant="primary">¡Haz clic aquí!</Button>
+      <NavBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
